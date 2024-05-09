3b5399bd69661c674f1f1146af6acc454448a24b
Hafsteinn Már Heimisson <haddi77@gmail.com>
2024-05-09 00:08:10 +0000

    profile update form almost complete

--- job_hunters/admin.py
+++ job_hunters/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import UserProfile
 
+from .models import CompanyProfile
+
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(CompanyProfile)
